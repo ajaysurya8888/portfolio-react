2bea59d59ba06964dffbe091aabdd74411229fe8
ajaysurya8888 <ajaysurya.m98@gmail.com>
2021-01-19 20:26:03 +0530

    commit

---
+++ portfolio/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Projects from './Projects';
+
+class App extends Component{
+    state ={ displayBio:false};
+    toggleDisplayBio=() =>{
+        this.setState({displayBio:!this.state.displayBio});
+    }
+    render(){
+        
+    
+         return (
+             <div>
+             <h1>This is ajaysurya</h1>
+             <p>Iam an assistant system engineer trainee</p>
+                    { this.state.displayBio? (
+                        <div>
+                        <p>i work in tata consultancy services</p>
+                        <p>I am good in java</p>
+                        <p>i love to play keyboard</p>
+                        <button onClick={this.toggleDisplayBio}>show less</button>
+                        </div>
+                        ):(
+                            <div>
+                            <button onClick={this.toggleDisplayBio}>Read More</button>
+                            </div>
+                         )
+                    }
+                  <hr />
+             <Projects />
+             </div>
+            
+         );
+    }
+}
+
+export default App;
+
---
+++ portfolio/src/data/projects.js
@@ -0,0 +1,30 @@
+import project1 from '../assets/project1.png';
+import project2 from '../assets/project2.png';
+import project3 from '../assets/project3.png';
+
+const PROJECTS =[
+    {
+        id:1,
+        title:'pharmacare',
+        description:'Web Application for pharmacy management',
+        link:'https://github.com/ajaysurya8888/pharmacare',
+        image:project1
+    },
+    {
+        id:2,
+        title:'house price prediction',
+        description:'Prediction of house price in bangalore using machine learning',
+        link:'https://github.com/ajaysurya8888/house-price-prediction',
+        image:project2
+    },
+    {
+        id:3,
+        title:'courier management system',
+        description:'Tracking and viewing status of couriers using spring boot',
+        link:'https://github.com/ajaysurya8888/house-price-prediction',
+        image:project3
+    }
+    
+];
+
+export default PROJECTS;
